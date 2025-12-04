6c94fbbe58d34e84dc88deb9ef02ed0220ecb323
liuziting <liuziting2014@163.com>
2025-12-04 09:44:07 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
     <div id="app" class="min-h-screen">
         <router-view />
         <!-- <FloatingDonation /> -->
+        <GlobalNoticeModal />
     </div>
 </template>
 
 <script setup>
 // App组件
 // import FloatingDonation from './components/FloatingDonation.vue'
+import GlobalNoticeModal from './components/GlobalNoticeModal.vue'
 </script>
